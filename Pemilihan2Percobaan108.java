c1a68ea4b35c5241e95ccadcff4acc72c16b01de
deaaww <deamarsell12@gmail.com>
2024-10-04 10:44:24 +0700

    simpan kode java ke Github

--- Pemilihan2Percobaan108.java
+++ Pemilihan2Percobaan108.java
@@ -11,14 +11,15 @@ public class Pemilihan2Percobaan108 {
         tahun = input08.nextInt();
 
         if ((tahun % 4) == 0) {
+            System.out.println("Tahun Kabisat");
             if ((tahun % 100) == 0) {
                 if ((tahun % 400) == 0) {
-                    System.out.println("Tahun Kabisat");
+
                 } else {
                     System.out.println("Bukan Tahun Kabisat");
+                    input08.close();
+                    return;
                 }
-            } else {
-                System.out.println("Tahun Kabisat");
             }   
         } else {
             System.out.println("Bukan Tahun Kabisat");
